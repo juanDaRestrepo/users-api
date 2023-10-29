179163c82ce35404839ae418dcd0859078acfe2f
Daniel Restrepo <jdrestrepo@unitecnica.net>
2023-10-29 18:36:12 -0500

    Finish initial config and env vars

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = process.env.NODE_ENV === 'production' ? 3000 : 4000;
+
+  app.enableCors(CORS)
+
+  app.setGlobalPrefix('api');
+  
+  await app.listen(port);
+  console.log(`Application running on: ${await app.getUrl()}`)
 }
 bootstrap();
